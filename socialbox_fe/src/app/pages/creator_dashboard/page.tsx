9561eb4e7cc22e8bc287f62e851fab3bf3448654
Ali-Mirghafouri <ali.mirghafouri@gmail.com>
2023-11-23 22:27:39 +0800

    added delete

--- socialbox_fe/src/app/pages/creator_dashboard/page.tsx
+++ socialbox_fe/src/app/pages/creator_dashboard/page.tsx
@@ -23,22 +23,33 @@ export const CreatorDashboard = () => {
     type: string;
     description: string;
     url: any;
+    index: number;
   }
 
-  const Card = ({ url, name, description, type }: cardProps) => {
+  const Card = ({ url, name, description, type, index }: cardProps) => {
     console.log(type === tabs[tab]);
     console.log(type);
+
+    let onDelete = (index: number) => {
+      let tempAssets = [...assets];
+      tempAssets.splice(index, 1);
+      console.log(tempAssets);
+      setAssets(tempAssets);
+    };
 
     // type === tabs[tab] ? (
     return type === tabs[tab] ? (
       <div key={name} className="dashboard_card">
-        <Image
-          className="dashboard_img"
-          src={url}
-          alt={name}
-        />
+        <Image className="dashboard_img" src={url} alt={name} />
         <div className="dashboard_img_title">{name}</div>
-        
+        <div className="dashboard_card_delete_container">
+          <button
+            onClick={() => onDelete(index)}
+            className="dashboard_mint_button"
+          >
+            Delete
+          </button>
+        </div>
       </div>
     ) : (
       <></>
@@ -64,8 +75,8 @@ export const CreatorDashboard = () => {
       </div>
       <hr />
       <div className="dashboard_container flex">
-        {assets?.map(({ name, description, url, type }: any) => (
-          <Card name={name} url={img.preview} type={type} />
+        {assets?.map(({ name, description, url, type }: any, index: number) => (
+          <Card name={name} url={img.preview} type={type} index={index} />
         ))}
       </div>
     </div>
